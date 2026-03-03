95d7b6c6c78569f3b8e2bea4bacd4199ce06b889
nurbelawahyuningse2327240105-ux <nurbelawahyuningse_2327240105@mhs.mdp.ac.id>
2026-03-03 14:28:54 +0700

    film

---
+++ film/lib/services/api_services.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = '789fef79f5e5b4945bce03faf442ecb7';
+
+  Future<List<Map<String, dynamic>>> getAllMovies() async {
+    final response = await http.get(
+      Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
+    );
+    final data = json.decode(response.body);
+    return List<Map<String, dynamic>>.from(data['results']);
+  }
+  
+  // getTrendingMovies
+  // getPopularMovies
+}
